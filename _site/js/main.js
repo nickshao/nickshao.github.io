31e18652e94c6059fd2b82315ae9eb754b2d7a10
邵楚荏 <nickshao@shaochurende-MacBook-Pro.local>
2019-02-22 16:00:07 +0800

    first_version push

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,19 +1,30 @@
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
+    $('.main-post-list').show()
+    $('.about-me').hide() 
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
+    if (currentWidth > 960) {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      $('.panel-cover').addClass('panel-cover--collapsed')
+    }
+  })
+  $('a.about-button').click(function (e) {
+    $('.main-post-list').hide()
+    $('.about-me').show()
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth > 960) {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      $('.panel-cover').addClass('panel-cover--collapsed')
     }
   })
 
-  if (window.location.hash && window.location.hash == '#blog') {
+  /*if (window.location.hash && window.location.hash == 'about-me') {
     $('.panel-cover').addClass('panel-cover--collapsed')
-  }
+  }*/
 
   if (window.location.pathname !== '/' && window.location.pathname !== '/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
